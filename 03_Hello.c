79d0d2ec1ce185b831e656bd1b6b9506b2283288
sahilthakur41 <2020a1r190@mietjammu.in>
2021-04-03 21:37:52 +0530

    hello world

---
+++ 03_Hello.c
@@ -0,0 +1,8 @@
+// SAHIL THAKUR
+//Program to print hello world.
+#include<stdio.h>//pre processor directive to inculde input output functions
+int main()//main funtion 
+{
+	printf("hello, world!");//Printing hello world
+	return 0;
+}
