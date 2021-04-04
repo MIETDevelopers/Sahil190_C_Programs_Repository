e646e8fa541cd8122bdb1720af0c28fc32f46993
sahilthakur41 <2020a1r190@mietjammu.in>
2021-04-04 11:17:08 +0530

    to read and write file

--- 30_ReadWriteFile.c
+++ 30_ReadWriteFile.c
@@ -1,35 +1,28 @@
-//C program to Program for reading and writing the student marks data to files.				
-#include <stdio.h>
-#include <stdlib.h>
-struct s {
-	//Creating a user defined data type using structures.
-	char name[50];
-	int RollNo;
-	int Marks;
-};
-int main() {
-	struct s a[10],b[10];
-	FILE *fptr;
-	int i;
-	fptr=fopen("./txt.txt","wb"); //Opening the file from the location.
-	for (i=0;i<1;++i) {
-		fflush(stdin);
-		//Scanning the entered Values.
-		printf("Enter Name: ");
-		gets(a[i].name);
-		printf("Enter Roll Number: ");
-		scanf("%d",&a[i].RollNo);
-		printf("Enter Marks: ");
-		scanf("%d",&a[i].Marks);
-	}
-	//Writing program.
-	fwrite(a,sizeof(a),1,fptr);
-	fclose(fptr);
-	fptr=fopen("./txt.txt","rb");
-	fread(b,sizeof(b),1,fptr);
-	printf("\n Entered details are:\n");
-	for (i=0;i<1;++i) {
-		printf("	Name: %s\n	Roll Number: %d\n	Marks: %d",b[i].name,b[i].RollNo,b[i].Marks); //Printing the entered text.
-	}
-	fclose(fptr); //Closing the program.
-}
+#include <stdio.h> // Pre-Process directive to include standard input output functions header file
+#include <stdlib.h> //Pre-Process directive to include standard library functions header file
+
+int main() //Main Function body starting 
+{
+   int num;
+   //Declaraing a pointer of type FILE. This declaration is needed for communication between the file and the program.
+   FILE *fptr; 
+
+   //Creating a new file named "newprogram.txt" and opening it for writing as per the mode 'w'.
+   fptr = fopen("D:\\C_Programs\\Sahil190_C_Programs_Repository\\program.txt","w");
+
+   //Checking whether the file is empty? if yes, it will return Error! and exits the file
+   {
+   if(fptr == NULL) 
+      printf("Error!");   
+      exit(1);  
+   }
+
+   printf("Enter num: ");
+   scanf("%d",&num);
+
+   fprintf(fptr,"%d",num);
+   fclose(fptr); //fclose function is used to close the file
+
+   return 0; //retun function
+
+} // Main function body ends
